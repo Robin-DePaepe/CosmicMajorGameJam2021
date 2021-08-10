b4a7cf5324f186c9b1591c002818010a97edba38
Sachin Kishan <sachinkishan02@gmail.com>
2021-08-10 18:07:14 +0530

    Added suspicion manager basic functions
    All managers have a static instance called "main"

---
+++ CosmicMajorGameJam2021/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int score;
+
+    public static ScoreManager main;
+
+    private void Awake()
+    {
+        main = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowScoreInDebug()
+    {
+        Debug.Log(score);
+    }
+
+    public void AddScore(int addition)
+    {
+        score += addition;
+    }
+
+    public void ReduceScore(int reduction)
+    {
+        score -= reduction;
+    }
+}
---
+++ CosmicMajorGameJam2021/Assets/Scripts/Managers/SuspicionManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuspicionManager : MonoBehaviour
+{
+    [Tooltip("At what value of suspicion does the player lose?")]
+    [SerializeField] private int suspicionLossValue;
+    
+    
+    private int suspicion;
+
+    public static SuspicionManager main;
+
+    private void Awake()
+    {
+        main = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddSuspicion(int addition)
+    {
+        suspicion += addition;
+        
+    }
+
+    public void ReduceSuspicion(int reduction)
+    {
+        suspicion -= reduction;
+    }
+
+    public void CheckForLossCondition()
+    {
+        if (suspicion >= suspicionLossValue)
+        {
+            //insert what happens when we lose here
+        }
+    }
+}
