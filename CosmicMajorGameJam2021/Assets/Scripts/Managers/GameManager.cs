8db99ac3427b08ac5f926a6406853af35e7c12b3
Sachin Kishan <sachinkishan02@gmail.com>
2021-08-11 18:58:09 +0530

    Added extended time manager, game manager to load scenes across main menu, game scene, loss scene, credits scene

---
+++ CosmicMajorGameJam2021/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager main;
+
+   /* private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadCreditScene()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void Loss()
+    {
+        //play loss sound effect
+        //play loss visual effects
+        SceneManager.LoadScene("EndScene");
+    }
+
+    public void Win()
+    {
+        
+    }
+}
