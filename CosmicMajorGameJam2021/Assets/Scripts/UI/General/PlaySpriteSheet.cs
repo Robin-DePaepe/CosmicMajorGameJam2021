6f75476d39fde9a671dfd18282198d39e067dbf6
Ryan Koning <rkoning8@gmail.com>
2021-08-12 12:28:30 -0600

    Added planet sprite sheets and thumbnails; Configured animations for each planet

---
+++ CosmicMajorGameJam2021/Assets/Scripts/UI/General/PlaySpriteSheet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySpriteSheet : MonoBehaviour
+{
+
+    public Image image;
+
+    public Texture2D spriteSheet;
+    private Sprite[] sprites;
+
+    private void Start()
+    {
+        //sprites = Resources.LoadAll<Sprite>(Texture.name)
+    }
+
+    void Update()
+    {
+        
+    }
+}
