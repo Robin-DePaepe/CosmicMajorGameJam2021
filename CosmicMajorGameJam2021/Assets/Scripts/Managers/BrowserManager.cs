2852e548a1823d0bd89cc8fdf533fdb813df5123
Lowskii <Lowskii@noreply.github.com>
2021-08-14 13:46:54 +0200

    created an email system with tabs, still need to add the making of the mod pages with download

---
+++ CosmicMajorGameJam2021/Assets/Scripts/Managers/BrowserManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrowserManager : MonoBehaviour
+{
+
+    #region properties
+    [SerializeField] private GameObject tabsParent;
+    [SerializeField] private GameObject tabPrefab;
+    [SerializeField] private GameObject addButton;
+    [SerializeField] private GameObject defaultPage;
+
+    const int maxTabs = 3;
+
+    List<GameObject> tabs = new List<GameObject>();
+    #endregion
+
+    private void Start()
+    {
+        AddNewTab();
+    }
+
+    public void AddNewTab()
+    {
+        GameObject tab = Instantiate(tabPrefab, tabsParent.transform);
+
+        tabs.Add(tab);
+        tab.GetComponent<TabBehaviour>().Page = Instantiate(defaultPage, transform);
+
+        int addButtonIndex = tabs.Count;
+
+        addButton.transform.SetSiblingIndex(addButtonIndex);
+        if (addButtonIndex == maxTabs) addButton.SetActive(false);
+    }
+
+    public void DisableTabPages()
+    {
+        foreach (GameObject tab in tabs)
+        {
+            tab.GetComponent<TabBehaviour>().DisablePage();
+        }
+    }
+
+    public void RemovePage(GameObject tab)
+    {
+        if (tabs.Contains(tab))
+        {
+            tabs.Remove(tab);
+            addButton.SetActive(true);
+
+            if (tabs.Count == 0) Destroy(this.gameObject);
+            else tabs[0].GetComponent<TabBehaviour>().ShowPage();
+
+        }
+    }
+}
---
+++ CosmicMajorGameJam2021/Assets/Scripts/UI/Windows/TabBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabBehaviour : MonoBehaviour
+{
+    #region Variables
+    private GameObject page;
+    private BrowserManager browserManager;
+
+    public GameObject Page
+    {
+        get { return page; }
+        set {
+            page = value; 
+        }
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        browserManager = transform.root.GetComponentInChildren<BrowserManager>();
+    }
+    public void Close()
+    {
+        browserManager.RemovePage(this.gameObject);
+        Destroy(page);
+        Destroy(this.gameObject);
+    }
+
+    public void ShowPage()
+    {
+        page.SetActive(true);
+    }
+
+     public void DisablePages()
+    {
+        browserManager.DisableTabPages();
+    }
+
+    public void DisablePage()
+    {
+        page.SetActive(false);
+    }
+}
