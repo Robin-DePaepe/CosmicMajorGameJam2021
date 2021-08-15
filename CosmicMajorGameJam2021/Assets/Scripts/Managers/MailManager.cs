3f320c1d6c4ba77b3d64a9c5f46f6be72d0113dc
Lowskii <Lowskii@noreply.github.com>
2021-08-15 02:45:48 +0200

    basics of the mail system

---
+++ CosmicMajorGameJam2021/Assets/Scripts/Managers/MailManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailManager : MonoBehaviour
+{
+    #region Variables
+    public static MailManager main;
+
+    private static List<Mail> mails = new List<Mail>();
+
+    static private TextAsset mailList;
+
+    #endregion
+
+    private void Start()
+    {
+        main = this;
+
+        StartCoroutine(ScheduleNewMail(10f, new Mail("titel", "robin DP", "wat ben jij goed")));
+        if (!mailList)
+        {
+            mailList = Resources.Load<TextAsset>("EmailChart");
+            CSVReader.LoadFromString(mailList.text, Reader);
+        }
+    }
+
+    public IEnumerator ScheduleNewMail(float time, Mail mail)
+    {
+        yield return new WaitForSeconds(time);
+
+        AddMail(mail);
+    }
+
+    private void AddMail(Mail mail)
+    {
+        mails.Add(mail);
+
+        //play sound
+        //fire pop up
+    }
+
+    private void Reader(int lineIndex, List<string> line)
+    {
+        Mail mail = new Mail(line[3], line[3], line[4]);
+
+        ScheduleNewMail(float.Parse(line[0]), mail);
+    }
+}
---
+++ CosmicMajorGameJam2021/Assets/Scripts/Other/Mail.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mail : MonoBehaviour
+{
+    #region Variables
+    private string title;
+    private string infoSender;
+    private string body;
+    #endregion
+
+    public Mail(string _title, string _info, string _body)
+    {
+        body = _body;
+        infoSender = _info;
+        title = _title;
+    }
+
+}
